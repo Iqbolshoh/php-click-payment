fdf1509256057d15714165528d57bd6c0e8de37e
iqbolshoh <iilhomjonov777@gmail.com>
2025-01-21 13:13:31 +0500

    commit

--- complate.php
+++ complate.php
@@ -14,17 +14,28 @@ $service_id = 'YOUR_SERVICE_ID';
 $merchant_user_id = 'YOUR_MERCHANT_USER_ID';
 $secret_key = 'YOUR_SECRET_KEY';
 
+$click_trans_id = isset($request['click_trans_id']) ? $request['click_trans_id'] : null;
+$service_id_request = isset($request['service_id']) ? $request['service_id'] : null;
+$merchant_trans_id = isset($request['merchant_trans_id']) ? $request['merchant_trans_id'] : null;
+$amount = isset($request['amount']) ? $request['amount'] : null;
+$action = isset($request['action']) ? $request['action'] : null;
+$error = isset($request['error']) ? $request['error'] : null;
+$error_note = isset($request['error_note']) ? $request['error_note'] : null;
+$sign_time = isset($request['sign_time']) ? $request['sign_time'] : null;
+$sign_string_request = isset($request['sign_string']) ? $request['sign_string'] : null;
+$merchant_prepare_id = isset($request['merchant_prepare_id']) ? $request['merchant_prepare_id'] : null;
+
 if (
-    !(isset($request['click_trans_id']) &&
-        isset($request['service_id']) &&
-        isset($request['merchant_trans_id']) &&
-        isset($request['amount']) &&
-        isset($request['action']) &&
-        isset($request['error']) &&
-        isset($request['error_note']) &&
-        isset($request['sign_time']) &&
-        isset($request['sign_string']) &&
-        isset($request['click_paydoc_id']))
+    !(isset($click_trans_id) &&
+        isset($service_id_request) &&
+        isset($merchant_trans_id) &&
+        isset($amount) &&
+        isset($action) &&
+        isset($error) &&
+        isset($error_note) &&
+        isset($sign_time) &&
+        isset($sign_string_request) &&
+        isset($merchant_prepare_id))
 ) {
     echo json_encode(array(
         'error' => -8,
@@ -34,17 +45,17 @@ if (
 }
 
 $sign_string = md5(
-    $request['click_trans_id'] .
-    $request['service_id'] .
+    $click_trans_id .
+    $service_id_request .
     $secret_key .
-    $request['merchant_trans_id'] .
-    $request['merchant_prepare_id'] .
-    $request['amount'] .
-    $request['action'] .
-    $request['sign_time']
+    $merchant_trans_id .
+    $merchant_prepare_id .
+    $amount .
+    $action .
+    $sign_time
 );
 
-if ($sign_string != $request['sign_string']) {
+if ($sign_string != $sign_string_request) {
     echo json_encode(array(
         'error' => -1,
         'error_note' => 'SIGN CHECK FAILED!'
@@ -52,7 +63,7 @@ if ($sign_string != $request['sign_string']) {
     exit;
 }
 
-if ((int) $request['action'] != 1) {
+if ((int) $action != 1) {
     echo json_encode(array(
         'error' => -3,
         'error_note' => 'Action not found'
@@ -60,8 +71,7 @@ if ((int) $request['action'] != 1) {
     exit;
 }
 
-$user_id = $request['merchant_trans_id'];
-if (!$user_id) {
+if (!$merchant_trans_id) {
     echo json_encode(array(
         'error' => -5,
         'error_note' => 'User does not exist'
@@ -69,7 +79,6 @@ if (!$user_id) {
     exit;
 }
 
-$merchant_prepare_id = $request['merchant_prepare_id'];
 if (!$merchant_prepare_id) {
     echo json_encode(array(
         'error' => -6,
@@ -77,9 +86,8 @@ if (!$merchant_prepare_id) {
     ));
     exit;
 } else {
-    $amount = $request['amount'];
     $time = time();
-    $trans_id = $request['click_trans_id'];
+    $trans_id = $click_trans_id;
 
     $existing_payment = $query->select('payments', '*', 'click_trans_id = ?', [$trans_id], 's');
 
@@ -119,7 +127,7 @@ if (!$merchant_prepare_id) {
     }
 }
 
-if ($request['error'] < 0) {
+if ($error < 0) {
     echo json_encode(array(
         'error' => -6,
         'error_note' => 'Transaction does not exist'
@@ -129,8 +137,8 @@ if ($request['error'] < 0) {
     echo json_encode(array(
         'error' => 0,
         'error_note' => 'Success',
-        'click_trans_id' => $request['click_trans_id'],
-        'merchant_trans_id' => $request['merchant_trans_id'],
+        'click_trans_id' => $click_trans_id,
+        'merchant_trans_id' => $merchant_trans_id,
         'merchant_confirm_id' => $log_id,
     ));
     exit;
--- prepare.php
+++ prepare.php
@@ -14,17 +14,28 @@ $service_id = 'YOUR_SERVICE_ID';
 $merchant_user_id = 'YOUR_MERCHANT_USER_ID';
 $secret_key = 'YOUR_SECRET_KEY';
 
+$click_trans_id = isset($request['click_trans_id']) ? $request['click_trans_id'] : null;
+$service_id_request = isset($request['service_id']) ? $request['service_id'] : null;
+$merchant_trans_id = isset($request['merchant_trans_id']) ? $request['merchant_trans_id'] : null;
+$amount = isset($request['amount']) ? $request['amount'] : null;
+$action = isset($request['action']) ? $request['action'] : null;
+$error = isset($request['error']) ? $request['error'] : null;
+$error_note = isset($request['error_note']) ? $request['error_note'] : null;
+$sign_time = isset($request['sign_time']) ? $request['sign_time'] : null;
+$sign_string_request = isset($request['sign_string']) ? $request['sign_string'] : null;
+$click_paydoc_id = isset($request['click_paydoc_id']) ? $request['click_paydoc_id'] : null;
+
 if (
-    !(isset($request['click_trans_id']) &&
-        isset($request['service_id']) &&
-        isset($request['merchant_trans_id']) &&
-        isset($request['amount']) &&
-        isset($request['action']) &&
-        isset($request['error']) &&
-        isset($request['error_note']) &&
-        isset($request['sign_time']) &&
-        isset($request['sign_string']) &&
-        isset($request['click_paydoc_id']))
+    !(isset($click_trans_id) &&
+        isset($service_id_request) &&
+        isset($merchant_trans_id) &&
+        isset($amount) &&
+        isset($action) &&
+        isset($error) &&
+        isset($error_note) &&
+        isset($sign_time) &&
+        isset($sign_string_request) &&
+        isset($click_paydoc_id))
 ) {
     echo json_encode(array(
         'error' => -8,
@@ -34,16 +45,16 @@ if (
 }
 
 $sign_string = md5(
-    $request['click_trans_id'] .
-    $request['service_id'] .
+    $click_trans_id .
+    $service_id_request .
     $secret_key .
-    $request['merchant_trans_id'] .
-    $request['amount'] .
-    $request['action'] .
-    $request['sign_time']
+    $merchant_trans_id .
+    $amount .
+    $action .
+    $sign_time
 );
 
-if ($sign_string != $request['sign_string']) {
+if ($sign_string != $sign_string_request) {
     echo json_encode(array(
         'error' => -1,
         'error_note' => 'SIGN CHECK FAILED!'
@@ -51,7 +62,7 @@ if ($sign_string != $request['sign_string']) {
     exit;
 }
 
-if ((int) $request['action'] != 0) {
+if ((int) $action != 0) {
     echo json_encode(array(
         'error' => -3,
         'error_note' => 'Action not found'
@@ -59,7 +70,6 @@ if ((int) $request['action'] != 0) {
     exit;
 }
 
-$merchant_trans_id = $request['merchant_trans_id'];
 if (!$merchant_trans_id) {
     echo json_encode(array(
         'error' => -5,
@@ -68,9 +78,9 @@ if (!$merchant_trans_id) {
     exit;
 } else {
     $payment_data = [
-        'amount' => $request['amount'],
+        'amount' => $amount,
         'time' => date('Y-m-d H:i:s', time()),
-        'click_trans_id' => $request['click_trans_id'],
+        'click_trans_id' => $click_trans_id,
         'status' => 'unpay'
     ];
 
@@ -88,8 +98,8 @@ if (!$merchant_trans_id) {
 echo json_encode(array(
     'error' => 0,
     'error_note' => 'Success',
-    'click_trans_id' => $request['click_trans_id'],
-    'merchant_trans_id' => $request['merchant_trans_id'],
+    'click_trans_id' => $click_trans_id,
+    'merchant_trans_id' => $merchant_trans_id,
     'merchant_prepare_id' => $log_id,
 ));
 
