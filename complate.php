a934f4ed13451be06de32910b0c02faeb13a9786
iqbolshoh <iilhomjonov777@gmail.com>
2025-01-21 11:26:54 +0500

    commit

--- complate.php
+++ complate.php
@@ -1,6 +1,5 @@
 <?php
 
-require_once 'config.php';
 
 error_reporting(0);
 header('Content-Type: text/json');
@@ -13,9 +12,10 @@ $service_id = 'SIZNING_SERVICE_ID';
 $merchant_user_id = 'SIZNING_MERCHANT_USER_ID';
 $secret_key = 'SIZNING_SECRET_KEY';
 
-// So'rovda barcha parametrlar mavjudligini tekshirish
-if (
-    !(
+file_put_contents('complete_log.txt', $request);
+$myfile = fopen("newfile_complete.txt", "w") or die("Unable to open file!");
+// Проверка отправлено-ли все параметры
+if(!(
         isset($request['click_trans_id']) &&
         isset($request['service_id']) &&
         isset($request['merchant_trans_id']) &&
@@ -26,93 +26,145 @@ if (
         isset($request['sign_time']) &&
         isset($request['sign_string']) &&
         isset($request['click_paydoc_id'])
-    )
-) {
-    echo json_encode(array(
+    )){
+
+    echo json_encode( array(
         'error' => -8,
-        'error_note' => 'Clickdan so\'rovda xato mavjud'
+        'error_note' => 'Error in request from click'
     ));
+
     exit;
 }
 
-$query = new Database();
-
-// Xeshni tekshirish (so'rovning xavfsizligini tasdiqlash)
-$sign = $request['click_trans_id'] .
+// Проверка хеша
+$sign_string = md5(
+    $request['click_trans_id'] .
     $request['service_id'] .
     $secret_key .
     $request['merchant_trans_id'] .
+    $request['merchant_prepare_id'] .
     $request['amount'] .
     $request['action'] .
-    $request['sign_time'];
+    $request['sign_time']
+);
+// check sign string to possible
+if($sign_string != $request['sign_string']){
 
-$sign_string = md5($sign);
-
-// Xeshni tekshirish
-if ($sign_string != $request['sign_string']) {
-    echo json_encode(array(
+    echo json_encode( array(
         'error' => -1,
-        'error_note' => 'XESH TEKSHIRUVI XATO!'
+        'error_note' => 'SIGN CHECK FAILED!'
     ));
+
     exit;
 }
 
-// Action parametrini tekshirish
-if ((int) $request['action'] != 0) {
-    echo json_encode(array(
+if ((int)$request['action'] != 1 ) {
+
+    echo json_encode( array(
         'error' => -3,
-        'error_note' => 'Action topilmadi'
+        'error_note' => 'Action not found'
     ));
+
     exit;
 }
 
-// Merchant_trans_id bo'yicha foydalanuvchini tekshirish
+// merchant_trans_id - это ID пользователья который он ввел в приложении
+// Здесь нужно проверить если у нас в базе пользователь с таким ID
+
 $user = $request['merchant_trans_id'];
-if (!$user) {
-    echo json_encode(array(
+if(!$user){
+    echo json_encode( array(
         'error' => -5,
-        'error_note' => 'Foydalanuvchi topilmadi'
+        'error_note' => 'User does not exist'
     ));
+
     exit;
 }
 
-$user_data = $query->select("users", "*", "username = ?", [$user], "s");
+//
 
-if (is_array($user_data) && count($user_data) > 0) {
-    $row = $user_data[0];
-    $name = $row['full_name'];
-    $username = $user;
-    $login = $row['login'];
-    $password = $row['password'];
+$prepared = $request['merchant_prepare_id'];
 
-    // Foydalanuvchining ma'lumotlarini asosiysi jadvalga qo'shish
-    $data = [
-        'full_name' => $name,
-        'username' => $username,
-        'password' => $password,
-        'role' => 'user'
-    ];
+if(!$prepared){
+    echo json_encode( array(
+        'error' => -6,
+        'error_note' => 'Transaction does not exist'
+    ));
 
-    $user_id = $query->insert("users", $data);
+    exit;
+}
+else{
+    $summa = $request['amount'];
+    $vaqt = time();
+    $trans_id = $request['click_trans_id'];
+    $url = "MANZIL";
+    $host = "HOST";
+    $user_d = "USER";
+    $password = "PAROL";
+    $db = "DATA_BASE_NAME";
+    $link = mysqli_connect($host, $user_d, $password, $db);
+    if (!$link) {
+        fwrite($myfile, "Xato: MySQL bilan aloqa o'rnatib bo'lmadi." . PHP_EOL);
+        fwrite($myfile, "Errno xato kodi: " . mysqli_connect_errno() . PHP_EOL);
+        fwrite($myfile, "Xatolik matni: " . mysqli_connect_error() . PHP_EOL);
+        exit();
+    }
+    else{
+        $sql = mysqli_query($link,"INSERT INTO tulovlar (user,summa, vaqt, trans_id) VALUES ('$user','$summa', '$vaqt', '$trans_id')");
+        if($sql==true){
+            fwrite($myfile,"sql2 ishladi userga kirdim \n");
+        }
+        else{
+            fwrite($myfile,"sql2 ishlamadi userga kirolmadim \n");
+        }
+        $sql = mysqli_query($link, "SELECT * from tulovlar WHERE telefon='$telefon' order by id desc");
+        if($sql==true){
+            fwrite($myfile,"sql3 ishladi userga kirdim \n");
+        }
+        else{
+            fwrite($myfile,"sql3 ishlamadi userga kirolmadim \n");
+        }
+        $data = mysqli_fetch_array($sql, MYSQLI_BOTH);
+        $log_id = $data['id'];
+        fwrite($myfile,"logid=".$log_id." \n");
+    }
+}
 
-    $data = $query->select('users', '*', "username = ?", [$username], "s");
-    $log_id = $data[0]['id'];
+// Если это заказ тогда нужно проверить еще статус заказа, все еще заказ актуален или нет
+// если проверка не проходит то нужно возвращать ошибку -4
+
+// и еще нужно проверить сумму заказа
+// если не проходит тогда нужно возвращать ошибку -2
+
+// Еще одна проверка статуса заказа, не закрыть или нет
+// если заказ отменен тогда нужно возвращать ошибку - 9
+
+// Все проверки прошли успешно, тог здесь будем сохранять в базу что подготовка к оплате успешно прошла
+// можно сделать отдельную таблицу чтобы сохранить входящих данных как лог
+// и присвоит на параметр merchant_confirm_id номер лога
+//
+
+// Хотя все проверки выше были в prepare тоже, нужно убедится что еще раз проверить в complete
+
+// Ошибка деньги с карты пользователя не списались
+if( $request['error'] < 0 ) {
+    // делаем что нибудь (если заказ отменим заказ, если пополнение тогда добавим запись что пополненние не успешно)
+    echo json_encode( array(
+        'error' => -6,
+        'error_note' => 'Transaction does not exist'
+    ));
+
+    exit;
 } else {
-    echo json_encode(array(
-        'error' => -5,
-        'error_note' => 'Foydalanuvchi topilmadi'
+    // Все успешно прошел деньги списаны с карты пользователя тогда записываем в базу (сумма приходит в запросе)
+
+    echo json_encode( array(
+        'error' => 0,
+        'error_note' => 'Success',
+        'click_trans_id' => $request['click_trans_id'],
+        'merchant_trans_id' => $request['merchant_trans_id'],
+        'merchant_confirm_id' => $log_id,
     ));
+
     exit;
 }
-
-// Natijani JSON formatida qaytarish
-echo json_encode(array(
-    'error' => 0,
-    'error_note' => 'Muvaffaqiyatli',
-    'click_trans_id' => $request['click_trans_id'],
-    'merchant_trans_id' => $request['merchant_trans_id'],
-    'merchant_prepare_id' => $log_id,
-));
-
-exit;
-?>
--- prepare.php
+++ prepare.php
@@ -1,7 +1,5 @@
 <?php
 
-require_once 'config.php';
-
 error_reporting(0);
 header('Content-Type: text/json');
 header('Charset: UTF-8');
@@ -12,8 +10,9 @@ $merchant_id = 'SIZNING_MERCHANT_ID';
 $service_id = 'SIZNING_SERVICE_ID';
 $merchant_user_id = 'SIZNING_MERCHANT_USER_ID';
 $secret_key = 'SIZNING_SECRET_KEY';
-
-// So'rovda barcha parametrlar mavjudligini tekshirish
+file_put_contents('prepare_log.txt', $request);
+$myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
+// Проверка отправлено-ли все параметры
 if (
     !(
         isset($request['click_trans_id']) &&
@@ -28,16 +27,27 @@ if (
         isset($request['click_paydoc_id'])
     )
 ) {
+    fwrite('prepare_log.txt', "Error in request from click \n");
     echo json_encode(array(
         'error' => -8,
-        'error_note' => 'Clickdan so\'rovda xato mavjud'
+        'error_note' => 'Error in request from click'
     ));
+
     exit;
+} else {
+    fwrite($myfile, "trans_id=" . $request['click_trans_id'] . "\n");
+    fwrite($myfile, "service_id=" . $request['service_id'] . "\n");
+    fwrite($myfile, "merchant_trans_id=" . $request['merchant_trans_id'] . "\n");
+    fwrite($myfile, "amount=" . $request['amount'] . "\n");
+    fwrite($myfile, "action=" . $request['action'] . "\n");
+    fwrite($myfile, "error=" . $request['error'] . "\n");
+    fwrite($myfile, "error_note=" . $request['error_note'] . "\n");
+    fwrite($myfile, "sign_time=" . $request['sign_time'] . "\n");
+    fwrite($myfile, "sign_string=" . $request['sign_string'] . "\n");
+    fwrite($myfile, "click_paydoc_id=" . $request['click_paydoc_id'] . "\n");
 }
 
-$query = new Database();
-
-// Xeshni tekshirish (so'rovning xavfsizligini tasdiqlash)
+// Проверка хеша
 $sign = $request['click_trans_id'] .
     $request['service_id'] .
     $secret_key .
@@ -47,72 +57,105 @@ $sign = $request['click_trans_id'] .
     $request['sign_time'];
 
 $sign_string = md5($sign);
-
-// Xeshni tekshirish
+// check sign string to possible
 if ($sign_string != $request['sign_string']) {
+
     echo json_encode(array(
         'error' => -1,
-        'error_note' => 'XESH TEKSHIRUVI XATO!'
+        'error_note' => 'SIGN CHECK FAILED!'
     ));
+    fwrite('prepare_log.txt', "SIGN CHECK FAILED! \n");
     exit;
+} else {
+    fwrite($myfile, "md5dan o'tdi \n");
 }
-
-// Action parametrini tekshirish
 if ((int) $request['action'] != 0) {
+
     echo json_encode(array(
         'error' => -3,
-        'error_note' => 'Action topilmadi'
+        'error_note' => 'Action not found'
     ));
+    fwrite('prepare_log.txt', "Action not found! \n");
     exit;
+} else {
+    fwrite($myfile, "Actiondan o'tdi \n");
 }
+// merchant_trans_id - это ID пользователья который он ввел в приложении
+// Здесь нужно проверить если у нас в базе пользователь с таким ID
 
-// Merchant_trans_id bo'yicha foydalanuvchini tekshirish
 $user = $request['merchant_trans_id'];
 if (!$user) {
+    fwrite($myfile, "Userdan o'tmadi \n");
     echo json_encode(array(
         'error' => -5,
-        'error_note' => 'Foydalanuvchi topilmadi'
+        'error_note' => 'User does not exist'
     ));
     exit;
-}
-
-$user_data = $query->select("users", "*", "username = ?", [$user], "s");
-
-if (is_array($user_data) && count($user_data) > 0) {
-    $row = $user_data[0];
-    $name = $row['full_name'];
-    $username = $user;
-    $login = $row['login'];
-    $password = $row['password'];
-
-    // Foydalanuvchining ma'lumotlarini asosiysi jadvalga qo'shish
-    $data = [
-        'full_name' => $name,
-        'username' => $username,
-        'password' => $password,
-        'role' => 'user'
-    ];
-
-    $user_id = $query->insert("users", $data);
-
-    $data = $query->select('users', '*', "username = ?", [$username], "s");
-    $log_id = $data[0]['id'];
 } else {
-    echo json_encode(array(
-        'error' => -5,
-        'error_note' => 'Foydalanuvchi topilmadi'
-    ));
-    exit;
+    $url = "MANZIL";
+    $host = "HOST";
+    $user_d = "USER";
+    $password = "PAROL";
+    $db = "DATA_BASE_NAME";
+    $link = mysqli_connect($host, $user_d, $password, $db);
+    if (!$link) {
+        fwrite($myfile, "Xato: MySQL bilan aloqa o'rnatib bo'lmadi." . PHP_EOL);
+        fwrite($myfile, "Errno xato kodi: " . mysqli_connect_errno() . PHP_EOL);
+        fwrite($myfile, "Xatolik matni: " . mysqli_connect_error() . PHP_EOL);
+        exit();
+    } else {
+        $sql = mysqli_query($link, "SELECT * from user_temp WHERE telefon='$user' order by id desc");
+        if ($sql == true) {
+            fwrite($myfile, "sql1 ishladi user tempga kirdim \n");
+        } else {
+            fwrite($myfile, "sql1 ishlamadi user tempga kirolmadim \n");
+        }
+        $row = mysqli_fetch_array($sql, MYSQLI_BOTH);
+        $name = $row['ism'];
+        $telefon = $user;
+        $login = $row['login'];
+        $parol = $row['parol'];
+        $faoliyat = $row['faoliyat'];
+        $rol = "user";
+        fwrite($myfile, "Malumotlar : $name ,$telefon, $login, $parol, $faoliyat  \n");
+        $sql = mysqli_query($link, "INSERT INTO user (ism,login,telefon,parol,faoliyat,rol) VALUES ('$name','$login','$telefon','$parol','$faoliyat','$rol')");
+        if ($sql == true) {
+            fwrite($myfile, "sql2 ishladi userga kirdim \n");
+        } else {
+            fwrite($myfile, "sql2 ishlamadi userga kirolmadim \n");
+        }
+        $sql = mysqli_query($link, "SELECT * from user WHERE telefon='$telefon' order by id desc");
+        if ($sql == true) {
+            fwrite($myfile, "sql3 ishladi userga kirdim \n");
+        } else {
+            fwrite($myfile, "sql3 ishlamadi userga kirolmadim \n");
+        }
+        $data = mysqli_fetch_array($sql, MYSQLI_BOTH);
+        $log_id = $data['id'];
+        fwrite($myfile, "logid=" . $log_id . " \n");
+    }
+    fwrite($myfile, "Userdan o'tdi \n");
 }
+// Если это заказ тогда нужно проверить еще статус заказа, все еще заказ актуален или нет
+// если проверка не проходит то нужно возвращать ошибку -4
 
-// Natijani JSON formatida qaytarish
-echo json_encode(array(
+// и еще нужно проверить сумму заказа
+// если не проходит тогда нужно возвращать ошибку -2
+
+// Еще одна проверка статуса заказа, не закрыть или нет
+// если заказ отменен тогда нужно возвращать ошибку - 9
+
+// Все проверки прошли успешно, тог здесь будем сохранять в базу что подготовка к оплате успешно прошла
+// можно сделать отдельную таблицу чтобы сохранить входящих данных как лог
+// и присвоит на параметр merchant_prepare_id номер лога
+
+$myJSON = json_encode(array(
     'error' => 0,
-    'error_note' => 'Muvaffaqiyatli',
+    'error_note' => 'Success',
     'click_trans_id' => $request['click_trans_id'],
     'merchant_trans_id' => $request['merchant_trans_id'],
     'merchant_prepare_id' => $log_id,
 ));
-
+echo $myJSON;
+fwrite($myfile, $myJSON . "\n");
 exit;
-?>
