0c5a493aff6b961cc3ecdbcb9f7833c1f738729e
iqbolshoh <iilhomjonov777@gmail.com>
2025-01-22 23:48:35 +0500

    commit

--- index.php
+++ index.php
@@ -1,22 +1,3 @@
-<?php
-error_reporting(0);
-
-include 'config.php';
-$query = new Database();
-
-$transaction_id = date("Ymd_His");
-$payment_amount = 1000.00;
-
-$payment_data = [
-    'service_id' => SERVICE_ID,
-    'merchant_id' => MERCHANT_ID,
-    'amount' => $payment_amount,
-    'transaction_param' => $transaction_id
-];
-
-$payment_url = 'https://my.click.uz/services/pay?' . http_build_query($payment_data);
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
