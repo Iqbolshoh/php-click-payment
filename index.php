5d2edd018e93aa8b3bb58de19684dde594a56b1e
iqbolshoh <iilhomjonov777@gmail.com>
2025-01-22 23:54:02 +0500

    commit

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
+<?php
+error_reporting(0);
+
+include 'config.php';
+$query = new Database();
+
+$transaction_id = date("Ymd_His");
+$payment_amount = 1000.00;
+
+$payment_data = [
+    'service_id' => SERVICE_ID,
+    'merchant_id' => MERCHANT_ID,
+    'amount' => $payment_amount,
+    'transaction_param' => $transaction_id
+];
+
+$payment_url = 'https://my.click.uz/services/pay?' . http_build_query($payment_data);
+
+// Havolani yashirin shaklda saqlash
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -39,9 +59,9 @@
 </head>
 
 <body>
-    <a href="./redirect_payment.php" target="_blank" class="payment-button">
+    <button onclick="window.location.href='redirect_payment.php'" class="payment-button">
         <i></i>Pay with CLICK
-    </a>
+    </button>
 </body>
 
 </html>
