1643c73959a401e75d3fa344201c82629045558f
iqbolshoh <iilhomjonov777@gmail.com>
2025-02-23 19:53:12 +0500

    commit

--- database.sql
+++ database.sql
@@ -11,14 +11,3 @@ CREATE TABLE IF NOT EXISTS payments (
     status ENUM('unpay', 'paid') DEFAULT 'unpay',           -- Payment status
     date TIMESTAMP DEFAULT CURRENT_TIMESTAMP                -- Payment date and time
 );
-
-
-
-CREATE TABLE IF NOT EXISTS payments (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    amount DECIMAL(10, 2) NOT NULL,
-    click_trans_id VARCHAR(100) NOT NULL,
-    merchant_trans_id VARCHAR(100) NOT NULL,
-    status ENUM('unpay', 'paid') DEFAULT 'unpay',
-    time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
