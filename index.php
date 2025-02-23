b25b936cd52054dabc7d1166eebecb092f7c5557
iqbolshoh <iilhomjonov777@gmail.com>
2025-02-23 19:19:27 +0500

    commit

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
+<?php
+error_reporting(0);
+
+include 'config.php';
+$query = new Database();
+
+$transaction_id = "iqbolshoh_777";
+$payment_amount = 1000.00;
+
+if (isset($_POST['submit'])) {
+    $payment_data = [
+        'service_id' => SERVICE_ID,
+        'merchant_id' => MERCHANT_ID,
+        'amount' => $payment_amount,
+        'transaction_param' => $transaction_id
+    ];
+
+    $payment_url = 'https://my.click.uz/services/pay?' . http_build_query($payment_data);
+
+    header("Location: $payment_url");
+    exit;
+}
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -40,9 +63,11 @@
 </head>
 
 <body>
-    <button onclick="window.location.href='redirect_payment.php'" class="payment-button">
-        <i></i>Pay with CLICK
-    </button>
+    <form method="POST">
+        <button type="submit" name="submit" class="payment-button">
+            <i></i> Pay with CLICK
+        </button>
+    </form>
 </body>
 
 </html>
